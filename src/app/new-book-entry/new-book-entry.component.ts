4d5d0579b46c9ca96cc2851ae106a317e3ffc743
Ankit Gupta <ag3831124@gmail.com>
2019-11-06 13:49:59 +0530

    some change in new book entry

--- src/app/new-book-entry/new-book-entry.component.ts
+++ src/app/new-book-entry/new-book-entry.component.ts
@@ -1,28 +1,36 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit, ElementRef, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Barcode } from '../barcode.service';
 import { All } from '../app.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-new-book-entry',
   templateUrl: './new-book-entry.component.html',
   styleUrls: ['./new-book-entry.component.css'],
 })
-export class NewBookEntryComponent implements OnInit {
+export class NewBookEntryComponent implements OnInit , OnDestroy {
 isLoading = false;
+bookSubs: Subscription;
   constructor(private bar: Barcode, private app: All) { }
 onSubmit(form: NgForm) {
   this.isLoading = true;
   this.app.addBooks(form.value);
-  this.app.getBooksUpdateListener()
-.subscribe(() => {
-  this.isLoading = false;
-});
 }
 barcode( form: NgForm) {
   this.bar.barcodeGenerate(form.value.accession_no);
 }
 
 ngOnInit() {
-  }
+  this.bookSubs = this.app.getBooksUpdateListener()
+  .subscribe(() => {
+    this.isLoading = false;
+    alert('Book Saved');
+  });
+}
+
+ngOnDestroy() {
+  this.bookSubs.unsubscribe();
+}
+
 }
