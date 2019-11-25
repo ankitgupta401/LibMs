bc04922388abfd558afc9a48f0bdcd9549c58e5e
Ankit Gupta <ag3831124@gmail.com>
2019-11-25 17:07:12 +0530

    Fixed bugs in the Issue Book Page

--- src/app/issue-book/issue-book.component.ts
+++ src/app/issue-book/issue-book.component.ts
@@ -4,6 +4,7 @@ import { All } from '../app.service';
 import { Libcard } from '../Libcard.model';
 import { Books } from '../books.model';
 import { Subscription } from 'rxjs';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-issue-book',
@@ -13,8 +14,10 @@ import { Subscription } from 'rxjs';
 export class IssueBookComponent implements OnInit, OnDestroy {
 Libcard: Libcard;
 Books: Books[] = [];
+Books2: Books[] = [];
 today;
 date;
+lengthOfBooks = 0;
 isLoading = false;
 private bookSub: Subscription;
 private userSub: Subscription;
@@ -31,7 +34,7 @@ onSubmits(form: NgForm) {
   .subscribe(result => {
     if ( result.book.length > 0) {
     if ( !this.Books.find(m => m.accession_no === result.book[0].accession_no)) {
-this.Books.push(result.book[0]);
+this.Books2.push(result.book[0]);
 this.isLoading = false;
 } else {
   this.isLoading = false;
@@ -48,56 +51,55 @@ this.isLoading = false;
 }
 
 resetform2(form: NgForm) {
-  this.isLoading = true;
-  const cardNo = this.Libcard.cardNo;
   form.reset();
-  this.app.resetbooks();
-
-  this.app.getUser(cardNo);
+  this.Books2 = [];
 }
 resetform(form: NgForm) {
-  this.isLoading = true;
   form.reset();
   this.app.resetuser();
   this.app.resetbooks();
 
 }
 onIssue(form: NgForm) {
-  this.isLoading = true;
-  let isIssued = false;
-  if ( this.Libcard && this.Books.length > 0 ) {
-// tslint:disable-next-line: prefer-for-of
-for (let i = 0; i < this.Books.length; i++ ) {
-if (this.Books[i].borrowed !== true) {
-this.Books[i].borrower = this.Libcard.fname + ' ' + this.Libcard.lname;
-this.Books[i].cardNo = this.Libcard.cardNo;
-this.Books[i].borrowed = true;
-this.Books[i].borrow_date = this.date;
-this.Books[i].borrower_email = this.Libcard.email;
-this.Books[i].borrower_phone = this.Libcard.phone_no;
-this.Books[i].borrower_dept = this.Libcard.dept;
-this.app.issueBook(this.Books[i])
-.subscribe(() => {
-this.app.resetuser();
-this.app.resetbooks();
-this.isLoading = false;
-form.reset();
-});
 
-} else {
-  if (this.Books[i].cardNo !== this.Libcard.cardNo) {
-  isIssued = true;
-}
-}
-}
-}
-  if (isIssued ) {
-    this.isLoading = false;
-    alert('Some Books Are Already Issued To Others');
+  if ( this.Libcard && this.Books2.length > 0 ) {
+    this.isLoading = true;
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < this.Books2.length; i++ ) {
+      console.log(this.Libcard.cardNo);
+      console.log(this.Books2[i].cardNo);
+      if (this.Books2[i].cardNo !== '') {
+        this.isLoading = false;
+        alert('Some Books Are Already Issued To Others');
 
-} else {
-  this.isLoading = false;
-}
+        this.isLoading = false;
+        this.Books2 = [];
+        return;
+      }
+    }
+          // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < this.Books2.length; i++ ) {
+            if (this.Books2[i].borrowed !== true) {
+            this.Books2[i].borrower = this.Libcard.fname + ' ' + this.Libcard.lname;
+            this.Books2[i].cardNo = this.Libcard.cardNo;
+            this.Books2[i].borrowed = true;
+            this.Books2[i].borrow_date = this.date;
+            this.Books2[i].borrower_email = this.Libcard.email;
+            this.Books2[i].borrower_phone = this.Libcard.phone_no;
+            this.Books2[i].borrower_dept = this.Libcard.dept;
+            this.app.issueBook(this.Books2[i])
+            .subscribe(() => {
+              this.app.resetuser();
+              this.app.resetbooks();
+              this.Books2 = [];
+              this.Books = [];
+              this.isLoading = false;
+              form.reset();
+              alert('Book Issued');
+            });
+            }
+            }
+  }
 }
 
   ngOnInit() {
@@ -110,13 +112,20 @@ form.reset();
     });
      this.bookSub = this.app.getBooksUpdateListener()
     .subscribe(response => {
-      this.Books = response.BOOKS;
+      if (this.Libcard) {
+        this.Books = response.BOOKS;
+        this.lengthOfBooks = response.BOOKS.length;
+      }
       this.isLoading = false;
     });
 
 
   }
   ngOnDestroy() {
+    this.app.resetuser();
+    this.app.resetbooks();
+    this.Books2 = [];
+    this.isLoading = false;
     this.bookSub.unsubscribe();
     this.userSub.unsubscribe();
   }
